ddf6e1b18d4c05438b1ba9e06677e4f9ddc882c6
Jay Gondaliya <139247541+jayg2309@users.noreply.github.com>
2024-09-11 22:40:08 +0530

    Create AllDigits.java

---
+++ Basics/AllDigits.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class AllDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int temp = n;
+        int dig=0;
+        while(temp!=0){
+            temp=temp/10;
+            dig++;
+        }
+        int div = (int)Math.pow(10, dig-1); // 10**(dig-1)
+
+        while(div  !=0){
+            int q = n / div;
+            System.out.println(q);
+            n=n% div;
+            div = div/10;
+        }
+        sc.close();
+    }
+}
